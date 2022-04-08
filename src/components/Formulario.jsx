968083f5c1d378cc6bb6a75d220ffbae813d9b56
Jhan Bayer <jhanbayer@gmail.com>
2022-04-08 13:19:45 -0400

    I added styles to the input

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -15,6 +15,7 @@ const InputSubmit = styled.input`
   font-size: 20px;
   border-radius: 5px;
   transition: background-color 0.3s ease;
+  margin-top: 30px;
 
   &:hover {
     background-color: #7a7dfe;
