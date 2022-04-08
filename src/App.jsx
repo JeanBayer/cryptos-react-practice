17842bebe0b2493ddd5775f65a23e3e1f5e857e1
Jhan Bayer <jhanbayer@gmail.com>
2022-04-08 12:51:45 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import styled from "@emotion/styled";
+import ImagenCripto from "./img/imagen-criptos.png";
+
+import Formulario from "./components/Formulario";
+
+const Conetenedor = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width: 90%;
+  @media (min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`;
+const Imagen = styled.img`
+  max-width: 400px;
+  width: 80%;
+  margin: 100px auto 0 auto;
+  display: block;
+`;
+const Heading = styled.h1`
+  font-family: "Lato", sans-serif;
+  color: #fff;
+  text-align: center;
+  font-weight: 700;
+  margin-top: 80px;
+  margin-bottom: 50px;
+  font-size: 34px;
+
+  &::after {
+    content: "";
+    width: 100px;
+    height: 6px;
+    background-color: #66a2fe;
+    display: block;
+    margin: 10px auto 0 auto;
+  }
+`;
+
+function App() {
+  return (
+    <Conetenedor>
+      <Imagen src={ImagenCripto} alt="Imagen cripto" />
+      <div>
+        <Heading>Cotiza tu Crypto</Heading>
+        <Formulario />
+      </div>
+    </Conetenedor>
+  );
+}
+
+export default App;
